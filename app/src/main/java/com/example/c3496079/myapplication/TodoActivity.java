77cd804241d490e88f9ba0117d7d22e38aae4a07
AnoshLokasher <43850611+AnoshLokasher@users.noreply.github.com>
2019-01-07 23:54:10 +0000

    lab2 complete

--- app/src/main/java/com/example/c3496079/myapplication/TodoActivity.java
+++ app/src/main/java/com/example/c3496079/myapplication/TodoActivity.java
@@ -1,91 +1,137 @@
 package com.example.c3496079.myapplication;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class TodoActivity extends AppCompatActivity {
 
+    private static final int IS_SUCCESS = 0;
     private String[] mTodos;
     private int mTodoIndex = 0;
 
     public static final String TAG = "TodoActivity";
+    private static final String IS_TODO_COMPLETE = "com.example.isTodoComplete";
     private static final String TODO_INDEX = "todoIndex";
 
-
-
-    // override to write the value of mTodoIndex into the Bundle with TODO_INDEX as its key
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
         savedInstanceState.putInt(TODO_INDEX, mTodoIndex);
     }
 
-
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        // call the super class onCreate to complete the creation of activity like
-        // the view hierarchy
         super.onCreate(savedInstanceState);
-
         Log.d(TAG, " *** Just to say the PC is in onCreate!");
-
-        // set the user interface layout for this Activity
-        // the layout file is defined in the project res/layout/activity_todo.xml file
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_todo);
 
 
-        // check for saved state due to changes such as rotation or back button
-        // and restore any saved state such as the todo index
         if (savedInstanceState != null){
             mTodoIndex = savedInstanceState.getInt(TODO_INDEX, 0);
         }
 
+        int todoIndex = getIntent().getIntExtra(TODO_INDEX, 0);
 
+        final TextView textViewTodo;
+        textViewTodo = (TextView) findViewById(R.id.textViewTodo);
 
-        // initialize member TextView so we can manipulate it later
-        final TextView TodoTextView;
-        TodoTextView = (TextView) findViewById(R.id.textViewTodo);
+        setTextViewComplete("");
 
-        // read the todo array from res/values/strings.xml
         Resources res = getResources();
         mTodos = res.getStringArray(R.array.todo);
-        // display the first task from mTodo array in the TodoTextView
-        TodoTextView.setText(mTodos[mTodoIndex]);
+        textViewTodo.setText(mTodos[mTodoIndex]);
 
         Button buttonNext;
         buttonNext = (Button) findViewById(R.id.buttonNext);
-        // OnClick listener for the  Next button
         buttonNext.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 mTodoIndex = (mTodoIndex + 1) % mTodos.length;
-                TodoTextView.setText(mTodos[mTodoIndex]);
+                textViewTodo.setText(mTodos[mTodoIndex]);
+                setTextViewComplete("");
 
             }
         });
-                Button buttonPrev;
-                buttonPrev = (Button) findViewById(R.id.buttonPrev);
-                // OnClick listener for the  Next button
-                buttonPrev.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        mTodoIndex = (mTodoIndex == 0) ? mTodos.length-1 :mTodoIndex -1;
-                        TodoTextView.setText(mTodos[mTodoIndex]);
+
+
+        Button buttonPrev;
+        buttonPrev = (Button) findViewById(R.id.buttonPrev);
+        // OnClick listener for the  Next button
+        buttonPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTodoIndex = (mTodoIndex == 0) ? mTodos.length-1 :mTodoIndex -1;
+                textViewTodo.setText(mTodos[mTodoIndex]);
+                setTextViewComplete("");
+
+
+        Button buttonTodoDetail = (Button) findViewById(R.id.buttonTodoDetail);
+        buttonTodoDetail.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = TodoDetailActivity.newIntent(TodoActivity.this, mTodoIndex);
+                startActivityForResult(intent, IS_SUCCESS);
 
             }
+
         });
+            }
+        });
+
     }
-}
+
+    @Override
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+        if (requestCode == IS_SUCCESS ){
+
+            if (intent != null) {
+                boolean isTodoComplete = intent.getBooleanExtra(IS_TODO_COMPLETE, false);
+                updateTodoComplete(isTodoComplete);
+            } else {
+                Toast.makeText(this, R.string.back_button_pressed, Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, R.string.request_code_mismatch,
+                    Toast.LENGTH_SHORT).show();
+        }
+
+
+
+    }
+
+    private void updateTodoComplete(boolean isTodoComplete) {
+        final TextView textViewTodo;
+        textViewTodo = (TextView) findViewById(R.id.textViewTodo);
+
+
+        if (isTodoComplete) {
+            textViewTodo.setBackgroundColor(
+                    ContextCompat.getColor(this, R.color.backgroundSuccess));
+            textViewTodo.setTextColor(
+                    ContextCompat.getColor(this, R.color.colorSuccess));
+            setTextViewComplete("\u2713");
+    }
+
+            }
+
+    private void setTextViewComplete(String message) {
+        final TextView textViewComplete;
+        textViewComplete = (TextView) findViewById(R.id.textViewComplete);
+        textViewComplete.setText(message);
+    }
+
+
+};
 
 
